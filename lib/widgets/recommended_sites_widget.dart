ba625b734de38db0b44153bb5353186e1ab39235
nazhify <143929524+NazhifFahmi@users.noreply.github.com>
2025-04-19 19:02:24 +0700

    update lagiii

--- lib/widgets/recommended_sites_widget.dart
+++ lib/widgets/recommended_sites_widget.dart
@@ -8,49 +8,54 @@ class RecommendedSitesWidget extends StatefulWidget {
 }
 
 class _RecommendedSitesWidgetState extends State<RecommendedSitesWidget> {
-  List<Map<String, dynamic>> _allSites = [
+  List<Map<String, dynamic>> _sportsSites = [
     {
-      'name': 'Flutter Dev',
-      'description': 'Dokumentasi dan tutorial untuk Flutter',
-      'imageUrl': 'https://storage.googleapis.com/cms-storage-bucket/6e19fee6b47b36ca613f.png',
-      'url': 'https://flutter.dev',
+      'name': 'ESPN',
+      'description':
+          'Situs berita olahraga internasional mencakup sepak bola, basket, F1, dan banyak lagi.',
+      'imageUrl':
+          'https://upload.wikimedia.org/wikipedia/commons/2/2f/ESPN_wordmark.svg',
+      'url': 'https://www.espn.com',
       'isFavorite': false,
     },
     {
-      'name': 'Dart Dev',
-      'description': 'Bahasa pemrograman Dart',
-      'imageUrl': 'https://dart.dev/assets/img/shared/dart/logo+text/horizontal/white.svg',
-      'url': 'https://dart.dev',
+      'name': 'Goal.com',
+      'description':
+          'Portal berita sepak bola yang menyajikan berita, hasil, klasemen, dan statistik.',
+      'imageUrl':
+          'https://upload.wikimedia.org/wikipedia/en/thumb/c/cb/Goal.com_logo.svg/1200px-Goal.com_logo.svg.png',
+      'url': 'https://www.goal.com',
       'isFavorite': false,
     },
     {
-      'name': 'TomTom Developer',
-      'description': 'API dan SDK untuk layanan lokasi',
-      'imageUrl': 'https://developer.tomtom.com/assets/tomtom-logo-black.svg',
-      'url': 'https://developer.tomtom.com',
+      'name': 'Bleacher Report',
+      'description':
+          'Menyediakan berita dan analisis mendalam tentang olahraga dan tim favorit.',
+      'imageUrl':
+          'https://upload.wikimedia.org/wikipedia/commons/1/1b/Bleacher_Report_logo.svg',
+      'url': 'https://bleacherreport.com',
       'isFavorite': false,
     },
     {
-      'name': 'GitHub',
-      'description': 'Platform hosting dan kolaborasi kode',
-      'imageUrl': 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
-      'url': 'https://github.com',
-      'isFavorite': false,
-    },
-    {
-      'name': 'Stack Overflow',
-      'description': 'Komunitas Q&A untuk programmer',
-      'imageUrl': 'https://cdn.sstatic.net/Sites/stackoverflow/Img/logo.svg',
-      'url': 'https://stackoverflow.com',
+      'name': 'Tirto.id - Olahraga',
+      'description':
+          'Portal berita lokal Indonesia yang juga menyajikan konten olahraga terkini.',
+      'imageUrl':
+          'https://upload.wikimedia.org/wikipedia/commons/2/29/Tirto.id_logo.svg',
+      'url': 'https://tirto.id/q/olahraga-qSW',
       'isFavorite': false,
     },
   ];
 
-  List<Map<String, dynamic>> _favorites = [];
-  bool _showFavoritesOnly = false;
+  bool _showOnlyFavorites = false;
 
   @override
   Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> displayedSites =
+        _showOnlyFavorites
+            ? _sportsSites.where((site) => site['isFavorite']).toList()
+            : _sportsSites;
+
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
@@ -84,97 +89,140 @@ class _RecommendedSitesWidgetState extends State<RecommendedSitesWidget> {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  SizedBox(width: 48), // Placeholder for alignment
+                  IconButton(
+                    icon: Icon(
+                      _showOnlyFavorites
+                          ? Icons.favorite
+                          : Icons.favorite_border,
+                      color: _showOnlyFavorites ? Colors.red : Colors.grey,
+                    ),
+                    tooltip:
+                        _showOnlyFavorites ? 'Lihat Semua' : 'Lihat Favorit',
+                    onPressed: () {
+                      setState(() {
+                        _showOnlyFavorites = !_showOnlyFavorites;
+                      });
+                    },
+                  ),
                 ],
               ),
             ),
             Expanded(
-              child: ListView.builder(
-                padding: EdgeInsets.all(16),
-                itemCount: _allSites.length,
-                itemBuilder: (context, index) {
-                  final site = _allSites[index];
-                  return Container(
-                    margin: EdgeInsets.symmetric(vertical: 8),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.blueAccent.withOpacity(0.1),
-                          blurRadius: 8,
-                          offset: Offset(0, 4),
-                        ),
-                      ],
-                    ),
-                    child: Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(16),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 150,
-                            width: double.infinity,
+              child:
+                  displayedSites.isEmpty
+                      ? Center(child: Text('Tidak ada situs favorit.'))
+                      : ListView.builder(
+                        padding: EdgeInsets.all(16),
+                        itemCount: displayedSites.length,
+                        itemBuilder: (context, index) {
+                          final site = displayedSites[index];
+                          final originalIndex = _sportsSites.indexOf(site);
+
+                          return Container(
+                            margin: EdgeInsets.symmetric(vertical: 8),
                             decoration: BoxDecoration(
-                              borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
-                              image: DecorationImage(
-                                image: NetworkImage(site['imageUrl']),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      site['name'],
-                                      style: TextStyle(
-                                        fontSize: 18,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    IconButton(
-                                      icon: Icon(
-                                        site['isFavorite'] ? Icons.favorite : Icons.favorite_border,
-                                        color: site['isFavorite'] ? Colors.red : null,
-                                      ),
-                                      onPressed: () => _toggleFavorite(site),
-                                    ),
-                                  ],
-                                ),
-                                SizedBox(height: 8),
-                                Text(site['description']),
-                                SizedBox(height: 16),
-                                ElevatedButton.icon(
-                                  onPressed: () => _launchURL(site['url']),
-                                  icon: Icon(Icons.open_in_new),
-                                  label: Text(
-                                    'Kunjungi Situs',
-                                    style: TextStyle(color: Colors.white),
-                                  ),
-                                  style: ElevatedButton.styleFrom(
-                                    minimumSize: Size(double.infinity, 40),
-                                    backgroundColor: Colors.blue.shade800,
-                                    shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                  ),
+                              borderRadius: BorderRadius.circular(16),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.blueAccent.withOpacity(0.1),
+                                  blurRadius: 8,
+                                  offset: Offset(0, 4),
                                 ),
                               ],
                             ),
-                          ),
-                        ],
+                            child: Card(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(16),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    height: 150,
+                                    width: double.infinity,
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.vertical(
+                                        top: Radius.circular(16),
+                                      ),
+                                      image: DecorationImage(
+                                        image: NetworkImage(site['imageUrl']),
+                                        fit: BoxFit.contain,
+                                      ),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(16.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Expanded(
+                                              child: Text(
+                                                site['name'],
+                                                style: TextStyle(
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                            ),
+                                            IconButton(
+                                              icon: Icon(
+                                                site['isFavorite']
+                                                    ? Icons.favorite
+                                                    : Icons.favorite_border,
+                                                color:
+                                                    site['isFavorite']
+                                                        ? Colors.red
+                                                        : null,
+                                              ),
+                                              onPressed: () {
+                                                setState(() {
+                                                  _sportsSites[originalIndex]['isFavorite'] =
+                                                      !_sportsSites[originalIndex]['isFavorite'];
+                                                });
+                                              },
+                                            ),
+                                          ],
+                                        ),
+                                        SizedBox(height: 8),
+                                        Text(site['description']),
+                                        SizedBox(height: 16),
+                                        ElevatedButton.icon(
+                                          onPressed:
+                                              () => _launchURL(site['url']),
+                                          icon: Icon(Icons.open_in_browser),
+                                          label: Text(
+                                            'Kunjungi Situs',
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                            ),
+                                          ),
+                                          style: ElevatedButton.styleFrom(
+                                            minimumSize: Size(
+                                              double.infinity,
+                                              40,
+                                            ),
+                                            backgroundColor:
+                                                Colors.blue.shade800,
+                                            shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(12),
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          );
+                        },
                       ),
-                    ),
-                  );
-                },
-              ),
             ),
           ],
         ),
@@ -182,35 +230,20 @@ class _RecommendedSitesWidgetState extends State<RecommendedSitesWidget> {
     );
   }
 
-  void _toggleFavorite(Map<String, dynamic> site) {
-    setState(() {
-      site['isFavorite'] = !site['isFavorite'];
-      
-      if (site['isFavorite']) {
-        // Tambahkan ke favorit jika belum ada
-        if (!_favorites.contains(site)) {
-          _favorites.add(site);
-        }
-      } else {
-        // Hapus dari favorit
-        _favorites.removeWhere((s) => s['url'] == site['url']);
-      }
-    });
-  }
-
   Future<void> _launchURL(String url) async {
     try {
-      if (await canLaunch(url)) {
-        await launch(url);
+      final uri = Uri.parse(url);
+      if (await canLaunchUrl(uri)) {
+        await launchUrl(uri);
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text('Tidak dapat membuka URL: $url')),
         );
       }
     } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error: $e')),
-      );
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Error: $e')));
     }
   }
 }
